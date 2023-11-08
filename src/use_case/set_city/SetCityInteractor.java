ce5fbd7267acca59a6950f4164c69e061378e406
michaelcantprogram <changdao.he@mail.utoronto.ca>
2023-11-08 17:17:42 -0500

    implemented files of set city use case

--- src/use_case/set_city/SetCityInteractor.java
+++ src/use_case/set_city/SetCityInteractor.java
@@ -1,4 +1,18 @@
 package use_case.set_city;
 
-public class SetCityInteractor {
+import use_case.set_city.SetCityInputBoundary;
+
+public class SetCityInteractor implements SetCityInputBoundary{
+
+    private SetCityOutputBoundary setCityPresenter;
+    private SetCityDataAccessInterface setCityDataAccessObject;
+
+    public SetCityInteractor(SetCityOutputBoundary setCityPresenter, SetCityDataAccessInterface setCityDataAccessObject) {
+        this.setCityPresenter = setCityPresenter;
+        this.setCityDataAccessObject = setCityDataAccessObject;
+    }
+
+    public void execute(SetCityInputBoundary setCityInputData){
+
+    }
 }
