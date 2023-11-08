28ac621fb9cd54a199ac5cee3007688d5ce86789
michaelcantprogram <changdao.he@mail.utoronto.ca>
2023-11-08 12:06:44 -0500

    Added input data, output data classes

---
+++ src/use_case/get_weather/GetWeatherInputData.java
@@ -0,0 +1,10 @@
+package use_case.get_weather;
+
+public class GetWeatherInputData {
+
+    private String city;
+
+    public GetWeatherInputData(String city) {this.city = city;}
+
+    public String getCity() {return city;}
+}
